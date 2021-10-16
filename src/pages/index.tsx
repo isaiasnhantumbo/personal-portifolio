58fff9acffaad54499db2ae4a87e1d37b41216d4
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-10-16 12:57:16 +0200

    :sparkles: added header
    added header navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
 import { Hero } from '../components/Hero';
+
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
@@ -11,7 +15,28 @@ export default function Home() {
         <meta name="description" content="Simple Portfolio page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <header>
+        <a href="#">Madelyn Torff</a>
+        <nav>
+          <ul>
+            <li>
+              <Link href="#">
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#projects">
+                <a>Projects</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#contacts">
+                <a>Contacts</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <main className={styles.main}>
         <Hero />
       </main>
