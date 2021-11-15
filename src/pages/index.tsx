b8f7fd3a6659dd7a8903053eb5af101db935ae6d
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-11-15 18:29:59 +0200

    :art: added footer and nav component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
+import Head from 'next/head';
 
 import { Hero } from '../components/Hero';
+import { Navigation } from '../components/Navigation';
 import { ProjectBox } from '../components/ProjectBox';
 import { ProjectBoxLeft } from '../components/ProjectBoxLeft';
 
 import styles from '../styles/Home.module.scss';
+import { Footer } from '../components/Footer';
 
 export default function Home() {
   return (
@@ -17,28 +17,7 @@ export default function Home() {
         <meta name="description" content="Simple Portfolio page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <a href="#">Madelyn Torff</a>
-        <nav>
-          <ul>
-            <li>
-              <Link href="#">
-                <a>About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#projects">
-                <a>Projects</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#contacts">
-                <a>Contacts</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Navigation />
       <main className={styles.main}>
         <Hero />
       </main>
@@ -50,32 +29,7 @@ export default function Home() {
         <ProjectBoxLeft imageSrc="/project-image.png" />
         <ProjectBox imageSrc="/project-image2.png" />
       </section>
-      <footer className={styles.footer}>
-        <div>
-          <Image
-            className={styles.image}
-            src="/icons/instagram.svg"
-            alt="Instagram Logo"
-            width="48"
-            height="48"
-          />
-          <Image
-            className={styles.image}
-            src="/icons/linkedin.svg"
-            alt="Linkedin Logo"
-            width="48"
-            height="48"
-          />
-          <Image
-            className={styles.image}
-            src="/icons/mail.svg"
-            alt="Email Logo"
-            width="48"
-            height="48"
-          />
-        </div>
-        <span>Madelyn Torff 2021 </span>
-      </footer>
+      <Footer />
     </div>
   );
 }
