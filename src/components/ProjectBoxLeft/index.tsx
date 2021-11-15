b13303978f7ef02dbafdc2f6275370799b93208f
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-11-15 17:42:50 +0200

    :art: added project left component
    added component to show project information with image in left side

---
+++ src/components/ProjectBoxLeft/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+
+import styles from './styles.module.scss';
+
+interface ProjectBoxProps {
+  imageSrc: string;
+}
+
+export function ProjectBoxLeft({ imageSrc }: ProjectBoxProps) {
+  return (
+    <div className={styles.container}>
+      <Image src={imageSrc} alt="Project image" width="496" height="524" />
+      <div className={styles.projectDetails}>
+        <h1>Project Name</h1>
+        <p>
+          I created this personal project in order to show how to create an
+          interface in Figma using a portfolio as an example.
+        </p>
+        <button>View Project</button>
+      </div>
+    </div>
+  );
+}
